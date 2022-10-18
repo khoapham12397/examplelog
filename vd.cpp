f675ce489bdfd69c227723d9bebdb6222b59104a
Pham Minh Khoa <khoa.pham12397@hcmut.edu.vn>
2022-10-18 11:01:31 +0700

    add solve func

--- vd.cpp
+++ vd.cpp
@@ -1,28 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
+void solve(){
+
+}
 int main(){
     cout << "ok" <<endl;
     cout << "hello" <<endl;        
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
